b8647686021dd12d380860ab852c073b6efeea43
vdugeri <verem.dugeri@gmail.com>
2019-07-07 15:16:01 +0100

    Updates

---
+++ precache-manifest.f0b35fcc83db32408d23fd32552d1e6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ca5f605047c7cc1fd4d0f1eaa209a3",
+    "url": "/monster-desk/index.html"
+  },
+  {
+    "revision": "ccfec7a9f5eb262317d2",
+    "url": "/monster-desk/static/css/main.0c78eb2a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-desk/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ccfec7a9f5eb262317d2",
+    "url": "/monster-desk/static/js/main.f5ea528f.chunk.js"
+  },
+  {
+    "revision": "abfa8f10b263bc2f48ac",
+    "url": "/monster-desk/static/js/runtime~main.d11fec75.js"
+  }
+]);
